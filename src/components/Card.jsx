10414bedefdcb625a28121ddba33df50fd9571c1
HARSHANALLURI2901 <harshavardhan.nalluri@slu.edu>
2024-11-29 02:48:37 +0000

    work done for week 6 lab

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-const Card = ({description, alt_description, id, user, urls, likes}) => {
-
-  const style = {
-    backgroundImage: `url(${urls.small})`
-  }
-  
+export default function Button({text, handleClick}) {
   return (
-    <div className="fl w-50 w-25-m w-20-l pa2">
-      <Link to={`/product/${id}`} className="db link dim tc"> 
-        <div style={style} alt="" className="w-100 db outline black-10 h4 cover"></div>
-        <dl className="mt2 f6 lh-copy">
-          <dt className="clip">Title</dt>
-          <dd className="ml0 black truncate w-100">{description ?? alt_description}</dd>
-          <dt className="clip">Artist</dt>
-          <dd className="ml0 gray truncate w-100">{user.first_name} {user.last_name}</dd>
-          <dt className="clip">Likes</dt>
-          <dd className="ml0 gray truncate w-100">{likes} Likes</dd>
-        </dl>
-      </Link>
-    </div>
+    <a href="a" className="f5 no-underline black bg-animate hover-bg-black">(text)
+      <span className="pl1">(text)</span>
+      </a>      
   )
 }
-
-export default Card;
